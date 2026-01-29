e0ad91a7a63f3658ae180d9ea4e4330fbc2a98e4
TheDigitalRoom User <flame1361@gmail.com>
2026-01-28 20:18:37 -0700

    Final Optimization: STUN + Smooth Streaming

--- public/StreamManager.js
+++ public/StreamManager.js
@@ -139,7 +139,11 @@ class StreamManager {
     createBroadcasterPeerConnection(peerId) {
         console.log('[STREAM] Creating Broadcaster PC for:', peerId);
         const pc = new RTCPeerConnection({
-            iceServers: [{ urls: 'stun:stun.l.google.com:19302' }]
+            iceServers: [
+                { urls: 'stun:stun.l.google.com:19302' },
+                { urls: 'stun:stun1.l.google.com:19302' },
+                { urls: 'stun:stun2.l.google.com:19302' }
+            ]
         });
 
         pc.onicecandidate = (event) => {
