130e14aea53aa2df217e453366e63ac31a514565
TheDigitalRoom User <flame1361@gmail.com>
2026-01-28 20:18:56 -0700

    Buttery Smooth: Triple-STUN Redundancy & 60FPS Bias Complete

--- public/StreamManager.js
+++ public/StreamManager.js
@@ -167,7 +167,11 @@ class StreamManager {
     createViewerPeerConnection(streamerId, fromId) {
         console.log('[STREAM] Creating Viewer PC for streamer:', streamerId);
         const pc = new RTCPeerConnection({
-            iceServers: [{ urls: 'stun:stun.l.google.com:19302' }]
+            iceServers: [
+                { urls: 'stun:stun.l.google.com:19302' },
+                { urls: 'stun:stun1.l.google.com:19302' },
+                { urls: 'stun:stun2.l.google.com:19302' }
+            ]
         });
 
         pc.onicecandidate = (event) => {
